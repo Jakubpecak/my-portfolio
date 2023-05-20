55ff7be2725b191e56aeec11850dcd0552c3b16e
Jakubpecak <kanaknakola021@gmail.com>
2023-05-20 19:30:26 +0200

    start set

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MyProfileComponent } from './components/main/my-profile/my-profile.component';
+import { PageNotFoundComponent } from './components/shared/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: MyProfileComponent
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationComponent } from './components/shared/navigation/navigation.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { MyProfileComponent } from './components/main/my-profile/my-profile.component';
+import { PageNotFoundComponent } from './components/shared/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FooterComponent,
+    MyProfileComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
